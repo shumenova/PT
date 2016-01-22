ae2813272412036e03c98e98765966c93bf1d68b
sulushash <sulushash_98@mail.ru>
2016-01-22 11:58:58 +0600

    first

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,11 +11,11 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             double a, b;                                      //create 2 numbers, which can be fractional
-            Console.WriteLine("Enter 2 numbers");             \\ask to set 2 numbers
-            a = Convert.ToDouble(Console.ReadLine());         \\convert number a into double type
-            b = Convert.ToDouble(Console.ReadLine());         \\convert number b into double type
-            Console.WriteLine("a + b ={0}", a + b);           \\making operation addition and the result withdraw to the screen
-            Console.ReadLine();                               \\waiting for the following actions
+            Console.WriteLine("Enter 2 numbers");             //ask to set 2 numbers
+            a = Convert.ToDouble(Console.ReadLine());         //convert number a into double type
+            b = Convert.ToDouble(Console.ReadLine());         //convert number b into double type
+            Console.WriteLine("a + b ={0}", a + b);           //making operation addition and the result withdraw to the screen
+            Console.ReadLine();                               //waiting for the following actions
 
 
         }
