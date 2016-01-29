5b614f0295efc39dbe47a6cb4f5cde078273b1a3
sulushash <sulushash_98@mail.ru>
2016-01-29 12:11:43 +0600

    lab 1.8

--- lab1.8/lab1.8/Program.cs
+++ lab1.8/lab1.8/Program.cs
@@ -6,21 +6,42 @@ using System.Threading.Tasks;
 
 namespace lab1._8
 {
-    class Data
+    class Student
     {
         public string FirstName;
         public string LastName;
         public string Male;
         public int Age;
-        public int GPA;
-         
+        private double GPA;
+
+        public Student(string FirstName, string LastName, string Male, int Age, double GPA)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Male = Male;
+            this.Age = Age;
+            this.GPA = GPA;
+        }
+        public override string ToString()
+        {
+            return this.FirstName + " " + this.LastName + " " + this.Male + " " + this.Age + " " + this.GPA;
+    } 
            
     }
     class Program
     {
         static void Main(string[] args)
         {
+            string FirstName = Console.ReadLine();
+            string LastName = Console.ReadLine(); 
+            string Male = Console.ReadLine();
+            int Age = int.Parse(Console.ReadLine());
+            double GPA= double.Parse(Console.ReadLine());
+            Student informa = new Student(FirstName, LastName, Male, Age, GPA);
+            Console.WriteLine(informa);
+            Console.ReadKey();
 
         }
+
     }
 }
